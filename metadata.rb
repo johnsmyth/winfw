9fb7bbe19891b8ed4c41deb93da0b69568ef1921
jsmyth <jsmyth@10thmagnitude.com>
2015-04-07 08:03:28 -0500

    version 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'fatjohnny@wastedbandwidth.net'
 license          'All rights reserved'
 description      'Installs/Configures windows firewall'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
